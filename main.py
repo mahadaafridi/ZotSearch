f95bf440ed487a99c35f8e3154a04d607a405d68
Nathan Hai Nguyen <nathahn9@uci.edu>
2025-05-14 07:15:23 +0000

    Added get_token_fields()

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import os
 import re
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Set
 from bs4 import BeautifulSoup
 from Posting import Posting
 
@@ -56,3 +56,49 @@ def get_info(file_path: str) -> Tuple[str]:
         data = json.load(file)
 
     return (data['url'], data['content'], data['encoding'])
+
+# Gets the fields of all tokens
+# Lowkey boof can prob simplify code
+def get_token_fields(soup: BeautifulSoup, tokens: Set[str]) -> Dict[str, List[str]]:
+    """
+    Gets all the fields that is associated with the provided token.
+
+    Args:
+        soup (BeautifulSoup): parsed html using bs4
+        token (Set[str]): set of tokens to be searched for
+    
+    Returns:
+        Dict[str, List[str]]: dictionary containing all tokens as keys and their fields as values
+    """
+    token_fields = {}
+
+    for token in tokens:
+        fields = []
+
+        # Find if token is in title field
+        for title in soup.find_all('title'):
+            if token in tokenize(title.get_text()):
+                fields.append('title')
+                break
+        
+        # Find if token in header field
+        for header in soup.find_all(['h1', 'h2', 'h3']):
+            if token in tokenize(header.get_text()):
+                fields.append('header')
+                break
+        
+        # Find if token in strong field
+        for strong in soup.find_all('strong'):
+            if token in tokenize(strong.get_text()):
+                fields.append('strong')
+                break
+        
+        # Find if token in body field
+        for paragraph in soup.find_all(['p', 'span', 'div']):
+            if token in tokenize(paragraph.get_text()):
+                fields.append('body')
+                break
+        
+        token_fields[token] = fields
+    
+    return token_fields
