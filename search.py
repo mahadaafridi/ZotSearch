038171ac129d6c43567b2eda7e1f78a043496e05
Nathan Hai Nguyen <nathahn9@uci.edu>
2025-05-29 19:04:05 +0000

    Remove stopwords from query when searching

--- search.py
+++ search.py
@@ -6,6 +6,27 @@ import mmap
 from typing import List, Dict, Set, Tuple
 from nltk.stem import PorterStemmer
 from collections import defaultdict
+import concurrent.futures
+
+STOP_WORDS = {"a","about","above","after","again","against","all","am",
+            "an","and","any","are","aren't","as","at","be","because","been",
+            "before","being","below","between","both","but","by","can't",
+            "cannot","could","couldn't","did","didn't","do","does","doesn't",
+            "doing","don't","down","during","each","few","for","from","further",
+            "had","hadn't","has","hasn't","have","haven't","having","he","he'd",
+            "he'll","he's","her","here","here's","hers","herself","him","himself",
+            "his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is",
+            "isn't","it","it's","its","itself","let's","me","more","most","mustn't",
+            "my","myself","no","nor","not","of","off","on","once","only","or","other",
+            "ought","our","ours	ourselves","out","over","own","same","shan't","she",
+            "she'd","she'll","she's","should","shouldn't","so","some","such","than",
+            "that","that's","the","their","theirs","them","themselves","then","there",
+            "there's","these","they","they'd","they'll","they're","they've","this","those",
+            "through","to","too","under","until","up","very","was","wasn't","we","we'd",
+            "we'll","we're","we've","were","weren't","what","what's","when","when's","where",
+            "where's","which","while","who","who's","whom","why","why's","with","won't","would",
+            "wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
+
 
 def timer(func):
     def wrapper(*args, **kwargs):
@@ -64,6 +85,7 @@ class Search:
             return self.index_files.get(first_char)
         return self.index_files.get('other')
 
+    @timer
     def _get_token_postings(self, token: str) -> List[Dict]:
         """
         Get postings for a specific token from the appropriate index file.
@@ -128,7 +150,8 @@ class Search:
         Returns:
             List[str]: List of stemmed tokens
         """
-        tokens = query.lower().split()
+        tokens = set(query.lower().split())
+        tokens = tokens - STOP_WORDS
         return [self.stemmer.stem(token) for token in tokens]
     
     @timer
@@ -261,6 +284,7 @@ class Search:
                 print(f"Query completed in {elapsed_time:.2f} milliseconds")
                 for i, result in enumerate(top_results, 1):
                     print(f"{i}. {result['url']} (Score: {result['score']:.2f})")
+
 if __name__ == '__main__':
     # Initialize search with the index directory and doc ID file
     search_engine = Search(
