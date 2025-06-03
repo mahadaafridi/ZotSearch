170cfa656e369d5ece98f94b6c08f09ce9fdc0fc
Nathan Hai Nguyen <nathahn9@uci.edu>
2025-06-03 10:34:57 -0700

    GUI

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from search import Search
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/search": {"origins": "http://localhost:8000"}})
+CORS(app, resources={r"/search": {"origins": "http://localhost:5000"}})
 
 search_engine = Search(
-    index_dir = "small_dev",
-    doc_id_file = "small_dev_doc_id.jsonl"
-)
+        index_dir="DEV_index",
+        doc_id_file="DEV_doc_id.jsonl"
+    )
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -18,9 +18,14 @@ def search():
 
     try:
         results = search_engine.search(query)
+        print(results)
         return jsonify(results)
     except Exception as e:
         return jsonify({"error": f"Search failed: {str(e)}"}), 500
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
